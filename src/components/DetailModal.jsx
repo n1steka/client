117bc3086a351e7c7aa59433bd8adf7f441ae461
n1steka <mongolzkarigun@gmail.com>
2023-11-07 10:04:16 +0800

    comment uudiig awah api

--- src/components/DetailModal.jsx
+++ src/components/DetailModal.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-
-export default function DetailModal({ title, _id  , name  , description , photo }) {
+export default function DetailModal({ title, _id, name, description, img }) {
+  const [comment, setComment] = useState([]);
   // const { id } = useParams();
   console.log(_id, "DETAILS  : id ");
+  axios
+    .get(`http://localhost:8000/api/v1/post/${_id}/comments`)
+    .then((res) => setComment(res.data.data))
+    .catch((err) => {
+      console.log(err);
+    });
   return (
     <div>
       <dialog id="my_modal_1" className="modal">
@@ -12,14 +19,8 @@ export default function DetailModal({ title, _id  , name  , description , photo 
           <span className="text-xl">{title}</span>
           <span className="text-xl"> description {description} </span>
           <span className="text-xl">{_id} </span>
-
-          <p className="py-4">
-            test modal
-          </p>
-          <img
-                src={`http://localhost:8000/uploads/${photo}`}
-                alt="Album"
-              />
+          <p className="py-4">test modal</p> <p>{img}</p>
+          <img src={`http://localhost:8000/uploads/${img}`} alt="Album" />
           <div className="modal-action">
             <form method="dialog">
               <button className="btn">Close</button>
