fb5694e12074934c84789c8068c37c6c3934aea6
n1steka <mongolzkarigun@gmail.com>
2023-11-07 11:33:52 +0800

    commment bichsen hunii neriig haruulah

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -6,8 +6,11 @@ export default function CommentList({ comment }) {
       {comment.map((el, i) => {
         return (
           <ul className="menu menu-xs bg-base-200 w-56 rounded-box">
-            <li>
-              <a> {el.comment}</a>
+            <li className="">
+              <a className=" my-2">
+                {" "}
+                нэр ...{el.createUser?.name} : {el.comment}
+              </a>
             </li>
           </ul>
         );
