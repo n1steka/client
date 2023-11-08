c9c9f3858daceed5ef91fa2205c152dc469ff736
n1steka <mongolzkarigun@gmail.com>
2023-11-08 13:47:16 +0800

    xD

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -2,15 +2,12 @@ import React from "react";
 
 export default function CommentList({ comment }) {
   return (
-    <div>
+    <div className="my-2">
       {comment.map((el, i) => {
         return (
           <ul className="menu menu-xs bg-base-200 w-56 rounded-box">
             <li className="">
-              <a className=" my-2">
-                {" "}
-                нэр ...{el.createUser?.name} : {el.comment}
-              </a>
+              нэр {el.createUser?.name} : {el.comment}
             </li>
           </ul>
         );
